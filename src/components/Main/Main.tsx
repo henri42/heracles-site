f1f94e2055bfd41dcf0870730d150f38c750aad4
Igor Papandinas <igor.papandinas@posteo.net>
2022-06-13 00:28:08 +0200

    feat: metadata added

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -11,19 +11,22 @@ const TernoaAddress = '5FWuM8Q3DRBAzu2PeqfmXqwdnegk8yiiMLXtZLB3dqJjomG8'
 const Main = () => <main className={classes.root}>
         <Separator />
         <p className={classes.intro}>
-            We aim at securing 12 blockchains within the 12 labors of Heracles. <br />
-            Each labors will be a staking opportunity for you to participate to a blockchain mining process. <br />
-            Our first node is the first labor of Heracles and is currently active on the Ternoa Network.
+            We aim at securing 12 blockchains within the 12 labors of Heracles.<br />
+            Earn rewards by helping Heracles on each staking opportunity.<br />
+            Participate in the next evolution of the internet.<br />
+            <br />
+            Our first node is the first labor of Heracles and is currently active on the Ternoa mainnet network.
         </p>
         <Separator />
         <Chain
             name='Heracles against the Nemean Lion'
-            description='Validator node on the blockain Ternoa'
+            description='Validator node on the Ternoa mainnet'
             chain='Ternoa'
             chainLogo={TernoaLogo}
             chainAddress={TernoaAddress}
             picture={NemeanLion}
         />
+        <p className={classes.teasing}>More nodes are coming...</p>
     </main>
 
 

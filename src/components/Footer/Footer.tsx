01ccf12a2ead31c479e2120f3fd6703642aa4793
Henri Gouttard <henri.gouttard@protonmail.com>
2022-06-15 00:18:05 +0200

    Add twitter to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import classes from './Footer.module.scss'
 
 const Footer = () => 
     <footer className={classes.root}>
-        2022 / heracles.works / contact@heracles.works
+        2022 / heracles.works / contact@heracles.works / <a href="https://twitter.com/heracles_works">@heracles_works</a>
     </footer>
 
 export default Footer

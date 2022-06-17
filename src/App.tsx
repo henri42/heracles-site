665cc0dae5812f8f746d20fee5f11d2631fc749c
Henri Gouttard <henri.gouttard@protonmail.com>
2022-06-17 13:50:07 +0200

    feat: Add Header icon

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import Header from './components/Header/Header'
 import Home from './pages/Home/Home'
 import Calculator from './pages/Calculator/Calculator'
 
+import CalculatorIcon from './assets/calculator.svg'
+
 import classes from './App.module.scss'
 
 const App = () => {
   const subdomain = window.location.host.split(".")[0]
+  const isCalculator = subdomain === 'calculator'
+  const headerIcon = isCalculator ? CalculatorIcon : undefined
+  const pageName =  isCalculator ? 'Ternoa Staking Rewards Calculator' : 'Heracles'
   return <div className={classes.root}>
-    <Header pageName='Ternoa Staking Rewards Calculator'/>
+    <Header pageName={pageName} icon={headerIcon}/>
     <div className={classes.container}>
-      {(subdomain === 'calculator') ? <Calculator /> : <Home />}
+      {isCalculator ? <Calculator /> : <Home />}
     </div>
     <Footer />
   </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,12 @@ import classes from './Header.module.scss'
 
 type Props = {
     pageName: string;
+    icon?: string;
 };
 
-const Header = ({ pageName }: Props) => 
+const Header = ({ pageName, icon }: Props) => 
     <header className={classes.root}>
+        {icon !== undefined && <img src={icon} alt={pageName + " icon"} />}
         {pageName}
     </header>
 

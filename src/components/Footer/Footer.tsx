c0ebe6f06244f83a8aa1b3cfaf8d9f885e0966e6
Henri Gouttard <henri.gouttard@protonmail.com>
2022-06-17 21:13:02 +0200

    feat: make footer links clickable

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import classes from './Footer.module.scss'
 
 const Footer = () => 
     <footer className={classes.root}>
-        2022 / heracles.works / contact@heracles.works / <a href="https://twitter.com/heracles_works">@heracles_works</a>
+        2022 /  <a href="https://heracles.works">heracles.works</a> / <a href="mailto:contact@heracles.works">contact@heracles.works</a> / <a href="https://twitter.com/heracles_works">@heracles_works</a>
     </footer>
 
 export default Footer
